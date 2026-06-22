398dbc513a450bd5ce52dc3717b5871f7cd9383c
Vicodev <quinterodev_09@outlook.com>
2026-06-22 16:13:28 -0700

    feat(accounts): add AccountRouter with CRUD operations for accounts

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import { env } from "./config.ts";
 import { MovementRouter } from "./routes/MovementRouter.ts";
 import { categoryRouter } from "./routes/categoryRouter.ts";
+import { AccountRouter } from "./routes/accountRouter.ts";
 
 const app = express();
 
@@ -11,6 +12,7 @@ app.use(express.json());
 
 app.use("/movements", MovementRouter);
 app.use("/categories", categoryRouter);
+app.use("/accounts", AccountRouter);
 
 app.listen(env.PORT, () => {
   console.log(`http://localhost:${env.PORT}`);
---
+++ src/routes/accountRouter.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { AccountController } from "../controllers/accountController.ts";
+
+export const AccountRouter = Router();
+
+AccountRouter.get("/", AccountController.getAllAccounts);
+AccountRouter.get("/:id", AccountController.getAccount);
+AccountRouter.post("/", AccountController.createAccount);
+AccountRouter.put("/:id", AccountController.updateAccount);
+AccountRouter.delete("/:id", AccountController.deleteAccount);
