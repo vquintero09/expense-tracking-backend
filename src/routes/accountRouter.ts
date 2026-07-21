7adfeb0bbfe39bfd177c440cdf7643f7e3c9b1fd
Vicodev <quinterodev_09@outlook.com>
2026-07-21 12:41:42 -0700

    feat(accounts): remove duplicate route for total balance in AccountRouter

--- src/routes/accountRouter.ts
+++ src/routes/accountRouter.ts
@@ -5,6 +5,8 @@ export const AccountRouter = Router();
 
 AccountRouter.get("/", AccountController.getAllAccounts);
 
+AccountRouter.get("/total-balance", AccountController.getTotalBalance);
+
 AccountRouter.get("/:id", AccountController.getAccount);
 
 AccountRouter.post("/", AccountController.createAccount);
@@ -12,8 +14,6 @@ AccountRouter.post("/", AccountController.createAccount);
 AccountRouter.put("/:id", AccountController.updateAccount);
 
 AccountRouter.delete("/:id", AccountController.deleteAccount);
-
-AccountRouter.get("/total-balance", AccountController.getTotalBalance);
 
 AccountRouter.get("/:id/movements", AccountController.getMovementsByAccount);
 
